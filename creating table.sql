f1f15e74f71aebd8898778376036ab25cd5d4c8c
arina0505 <37370156+arina0505@users.noreply.github.com>
2018-04-30 12:10:51 +0530

    Create creating table.sql

---
+++ creating table.sql
@@ -0,0 +1,3 @@
+create table arina(sl_no varchar(10),name varchar(10),roll_no varchar(10),dept varchar(10),age varchar(10),sex varchar(10),hgt_cm varchar(200),wgt_kg varchar(100));
+insert into arina(sl_no,name,roll_no,dept,age,sex,hgt_cm,wgt_kg) values("1","prami","15bec041","ECE","20","female","170","60");
+select * from arina;
